bdd51f4d8a2a065362d6f5f687ab4be3a165271f
panaresium <robert_akk@hotmail.com>
2025-06-14 20:29:42 +0700

    Update my_bookings.js

--- static/js/my_bookings.js
+++ static/js/my_bookings.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const bookingCardDiv = bookingItemClone.querySelector('.booking-card'); // Use the main card div
 
         // Apply conditional classes for status
-        bookingCardDiv.classList.remove('booking-completed', 'booking-cancelled', 'booking-rejected', 'booking-cancelled-by-admin', 'booking-cancelled-admin-acknowledged');
+        bookingCardDiv.classList.remove('booking-completed', 'booking-cancelled', 'booking-rejected', 'booking-cancelled-by-admin', 'booking-cancelled-admin-acknowledged', 'cancelled', 'system-cancelled-no-checkin');
         const statusClass = `booking-${booking.status.toLowerCase().replace(/_/g, '-')}`;
         bookingCardDiv.classList.add(statusClass);
 
